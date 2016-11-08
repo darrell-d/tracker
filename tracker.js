0a931e568ba549e71001b4f656d1711c18b70629
Darrell De Freitas <darrell.defreitas@gmail.com>
2016-11-07 21:20:21 -0500

    Simplifications

--- tracker.js
+++ tracker.js
@@ -1,17 +1,21 @@
 var trackedItems = [];
+
 $(document).ready(function () {
+    
     //Check if index is already in use
     var localStorageSet = checkLocalStorage("tracked");
+    
     if (localStorageSet) {
+        
         trackedItems = extractTrackedItems("tracked");
+        
         for (i = 0; i < trackedItems.length; i++) {
-            var pTag = document.createElement("p");
-            var taskName = document.createElement("span");
             var payload = trackedItems[i].split(":");
-            taskName.innerHTML = payload[0];
-            pTag.appendChild(taskName);
-            pTag.setAttribute("startTime", payload[1]);
-            $("#tracking").prepend(pTag);
+            
+            var task = addTask(payload[1],payload[0]);
+            
+
+            $("#tracking").prepend(task);
         }
     }
     $('#add').click(function (e) {
@@ -22,28 +26,11 @@ $(document).ready(function () {
         var taskName = $("#entry").val();
         trackedItems.push(taskName + ":" + currentTime);
         
-        //New item HTML
-        var taskHolder = document.createElement("p");
-        taskHolder.setAttribute("class", "taskHolder");
-        taskHolder.setAttribute("startTime", currentTime);
-
-        var close = document.createElement("i");
-        close.setAttribute("class", "close");
-        close.innerHTML = '\u2A2F';
-
-        var trackerName = document.createElement("span");
-        trackerName.setAttribute("class", "trackerName");
-        trackerName.innerHTML = taskName;
-
-        var timeElapsed = document.createElement("span");
-        timeElapsed.setAttribute("class", "timeElapsed");
-
-        taskHolder.appendChild(close);
-        taskHolder.appendChild(trackerName);
-        taskHolder.appendChild(timeElapsed);
+        var taskHolder = addTask(currentTime,taskName);
 
         $("#tracking").prepend(taskHolder);
-        $("#entry").val("")
+        $("#entry").val("");
+        
         localStorage.setItem("tracked", trackedItems);
     });
     $("#clear").click(function () {
@@ -53,6 +40,8 @@ $(document).ready(function () {
     });
     initTimer();
 });
+
+
 /***Functions***/
 function storageAvailable(type) {
     try {
@@ -97,15 +86,31 @@ function initTimer() {
     }, 1000);
 }
 
-function appendTime(pTag, startTime) {
-    var taskTimer = document.createElement("span");
-    taskTimer.innerHTML = Math.floor((Date.now() / 1000) - startTime);
-    if (pTag.childElementCount > 1) {
-        pTag.removeChild(pTag.lastChild)
-        pTag.appendChild(taskTimer);
-    }
-    else {
-        pTag.appendChild(taskTimer);
-    }
-    return pTag;
+function appendTime(htmlTag, startTime) {
+
+    htmlTag.lastChild.innerHTML = Math.floor(calculateTimeDifference(startTime, Date.now() / 1000));
+}
+
+function addTask(currentTime,taskName) {
+    //New item HTML
+    var taskHolder = document.createElement("p");
+    taskHolder.setAttribute("class", "taskHolder");
+    taskHolder.setAttribute("startTime", currentTime);
+    
+    var close = document.createElement("i");
+    close.setAttribute("class", "close");
+    close.innerHTML = '\u2A2F';
+    
+    var trackerName = document.createElement("span");
+    trackerName.setAttribute("class", "trackerName");
+    trackerName.innerHTML = taskName;
+    
+    var timeElapsed = document.createElement("span");
+    timeElapsed.setAttribute("class", "timeElapsed");
+    
+    taskHolder.appendChild(close);
+    taskHolder.appendChild(trackerName);
+    taskHolder.appendChild(timeElapsed);
+    
+    return taskHolder;
 }
