91dc32c16c7e1ea9302765275c57b68d23cf8412
Darrell De Freitas <darrell.defreitas@gmail.com>
2016-11-07 20:54:55 -0500

    fixing logic and semantic structure of adding new task

--- tracker.js
+++ tracker.js
@@ -16,15 +16,33 @@ $(document).ready(function () {
     }
     $('#add').click(function (e) {
         e.preventDefault();
-        var pTag = document.createElement("p");
+        
+        //capture time of click
         var currentTime = Math.floor(Date.now() / 1000);
-        var taskName = document.createElement("span");
-        taskName.innerHTML = $("#entry").val();
-        pTag.appendChild(taskName);
-        trackedItems.push($("#entry").val() + ":" + currentTime);
-        pTag.setAttribute("startTime", currentTime);
-        pTag = appendTime(pTag, currentTime);
-        $("#tracking").prepend(pTag);
+        var taskName = $("#entry").val();
+        trackedItems.push(taskName + ":" + currentTime);
+        
+        //New item HTML
+        var taskHolder = document.createElement("p");
+        taskHolder.setAttribute("class", "taskHolder");
+        taskHolder.setAttribute("startTime", currentTime);
+
+        var close = document.createElement("i");
+        close.setAttribute("class", "close");
+        close.innerHTML = '\u2A2F';
+
+        var trackerName = document.createElement("span");
+        trackerName.setAttribute("class", "trackerName");
+        trackerName.innerHTML = taskName;
+
+        var timeElapsed = document.createElement("span");
+        timeElapsed.setAttribute("class", "timeElapsed");
+
+        taskHolder.appendChild(close);
+        taskHolder.appendChild(trackerName);
+        taskHolder.appendChild(timeElapsed);
+
+        $("#tracking").prepend(taskHolder);
         $("#entry").val("")
         localStorage.setItem("tracked", trackedItems);
     });
@@ -73,14 +91,15 @@ function initTimer() {
     var intervalID = setInterval(function () {
         //Stop loop if no timer exist
         $('p').each(function () {
-            appendTime(this, $(this).attr("startTime"))
+            appendTime(this, $(this).attr("startTime"));
+
         });
     }, 1000);
 }
 
-function appendTime(pTag, currentTime) {
+function appendTime(pTag, startTime) {
     var taskTimer = document.createElement("span");
-    taskTimer.innerHTML = Math.floor((Date.now() / 1000) - currentTime);
+    taskTimer.innerHTML = Math.floor((Date.now() / 1000) - startTime);
     if (pTag.childElementCount > 1) {
         pTag.removeChild(pTag.lastChild)
         pTag.appendChild(taskTimer);
